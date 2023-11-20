57cc36c4091c49db4f4d87582f801089b6b83ceb
Cheng <jiagongkuruochi@163.com>
2023-11-20 19:13:27 +0800

    提交全局配置Long转String/时间转换

---
+++ src/main/java/com/korant/youya/workplace/config/WebConfig.java
@@ -0,0 +1,62 @@
+package com.korant.youya.workplace.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+/**
+ * @ClassName WebConfig
+ * @Description
+ * @Author chenyiqiang
+ * @Date 2023/7/24 9:31
+ * @Version 1.0
+ */
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * 全局配置Long转String
+     *
+     * @param converters
+     */
+    @Override
+    public void configureMessageConverters(@NotNull List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        simpleModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(dateTimeFormatter));
+        simpleModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(dateTimeFormatter));
+
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        simpleModule.addSerializer(LocalDate.class, new LocalDateSerializer(dateFormatter));
+        simpleModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(dateFormatter));
+
+        //添加此配置
+        objectMapper.registerModule(simpleModule);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        jackson2HttpMessageConverter.setObjectMapper(objectMapper);
+        converters.add(jackson2HttpMessageConverter);
+    }
+}
--- src/main/java/com/korant/youya/workplace/utils/HttpClientUtil.java
+++ src/main/java/com/korant/youya/workplace/utils/HttpClientUtil.java
@@ -3,16 +3,19 @@ package com.korant.youya.workplace.utils;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @ClassName HttpClientUtil
@@ -45,6 +48,41 @@ public class HttpClientUtil {
                 .uri(URI.create(url))
                 .timeout(Duration.ofSeconds(TIMEOUT))
                 .GET()
+                .build();
+
+        HttpResponse<String> httpResponse = null;
+        try {
+            httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+        if (null != httpResponse) {
+            int statusCode = httpResponse.statusCode();
+            if (HttpStatus.OK.value() == statusCode) {
+                return httpResponse.body();
+            }
+            return null;
+        }
+        return null;
+    }
+
+    /**
+     * 发送GET请求
+     */
+    public static String sentGet(String url, Map<String, Object> paramMap) {
+
+        String paramString = paramMap.entrySet().stream()
+                .map(e -> {
+                    return e.getKey() + "=" + URLEncoder.encode(e.getValue().toString(), StandardCharsets.UTF_8);
+                })
+                .collect(Collectors.joining("&"));
+
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .uri(URI.create(url + "?" + paramString))
+                .timeout(Duration.ofSeconds(TIMEOUT))
+                .method(HttpMethod.GET.name(), HttpRequest.BodyPublishers.ofString(JSON.toJSONString(paramMap)))
                 .build();
 
         HttpResponse<String> httpResponse = null;
