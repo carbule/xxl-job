3ce3122522266d06be038fbb130dd23797b2c8a4
Cheng <jiagongkuruochi@163.com>
2024-03-14 11:34:43 +0800

    更新accessTokenRunner运行逻辑

--- src/main/java/com/korant/youya/workplace/runner/AccessTokenRunner.java
+++ src/main/java/com/korant/youya/workplace/runner/AccessTokenRunner.java
@@ -4,6 +4,7 @@ import com.korant.youya.workplace.utils.WeChatUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.Executors;
@@ -21,12 +22,18 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class AccessTokenRunner implements ApplicationRunner {
 
+    private final Environment environment;
+
+    public AccessTokenRunner(Environment environment) {
+        this.environment = environment;
+    }
+
     @Override
     public void run(ApplicationArguments args) {
+        // 获取当前激活的环境
+        String activeProfile = environment.getActiveProfiles()[0];
+        log.info("当前运行环境为: " + activeProfile);
         WeChatUtil.refreshMiniProgramAccessToken();
-        WeChatUtil.refreshOfficialAccountAccessToken();
-        WeChatUtil.refreshJsapiTicket();
-
         ScheduledExecutorService accessTokenExecutor = Executors.newSingleThreadScheduledExecutor();
         Runnable accessTokenTask = new Runnable() {
             @Override
@@ -52,54 +59,59 @@ public class AccessTokenRunner implements ApplicationRunner {
         // 设置定时器，任务执行完成后等待5分钟，然后再执行下一次任务
         accessTokenExecutor.scheduleWithFixedDelay(accessTokenTask, 5, 5, TimeUnit.MINUTES);
 
-        ScheduledExecutorService officialAccountAccessTokenExecutor = Executors.newSingleThreadScheduledExecutor();
-        Runnable officialAccountAccessTokenTask = new Runnable() {
-            @Override
-            public void run() {
-                int maxRetries = 3;
-                int retryCount = 0;
-                while (retryCount < maxRetries) {
-                    try {
-                        WeChatUtil.refreshOfficialAccountAccessToken();
-                        break;
-                    } catch (Exception e) {
-                        log.error("定时刷新OfficialAccountAccessToken失败,异常信息:", e);
-                        retryCount++;
-                        if (retryCount == maxRetries) {
-                            log.error("定时刷新OfficialAccountAccessToken方法调用已达到最大重试次数");
-                        } else {
-                            log.error("正在进行第 " + retryCount + " 次重试");
-                        }
-                    }
-                }
-            }
-        };
-        // 设置定时器，任务执行完成后等待5分钟，然后再执行下一次任务
-        officialAccountAccessTokenExecutor.scheduleWithFixedDelay(officialAccountAccessTokenTask, 5, 5, TimeUnit.MINUTES);
+        if (!"dev".equals(activeProfile)) {
+            WeChatUtil.refreshOfficialAccountAccessToken();
+            WeChatUtil.refreshJsapiTicket();
 
-        ScheduledExecutorService jsTicketExecutor = Executors.newSingleThreadScheduledExecutor();
-        Runnable jsTicketTask = new Runnable() {
-            @Override
-            public void run() {
-                int maxRetries = 3;
-                int retryCount = 0;
-                while (retryCount < maxRetries) {
-                    try {
-                        WeChatUtil.refreshJsapiTicket();
-                        break;
-                    } catch (Exception e) {
-                        log.error("定时刷新JSTicket失败,异常信息:", e);
-                        retryCount++;
-                        if (retryCount == maxRetries) {
-                            log.error("定时刷新JSTicket方法调用已达到最大重试次数");
-                        } else {
-                            log.error("正在进行第 " + retryCount + " 次重试");
+            ScheduledExecutorService officialAccountAccessTokenExecutor = Executors.newSingleThreadScheduledExecutor();
+            Runnable officialAccountAccessTokenTask = new Runnable() {
+                @Override
+                public void run() {
+                    int maxRetries = 3;
+                    int retryCount = 0;
+                    while (retryCount < maxRetries) {
+                        try {
+                            WeChatUtil.refreshOfficialAccountAccessToken();
+                            break;
+                        } catch (Exception e) {
+                            log.error("定时刷新OfficialAccountAccessToken失败,异常信息:", e);
+                            retryCount++;
+                            if (retryCount == maxRetries) {
+                                log.error("定时刷新OfficialAccountAccessToken方法调用已达到最大重试次数");
+                            } else {
+                                log.error("正在进行第 " + retryCount + " 次重试");
+                            }
                         }
                     }
                 }
-            }
-        };
-        // 设置定时器，任务执行完成后等待2小时，然后再执行下一次任务
-        jsTicketExecutor.scheduleWithFixedDelay(jsTicketTask, 2, 2, TimeUnit.HOURS);
+            };
+            // 设置定时器，任务执行完成后等待5分钟，然后再执行下一次任务
+            officialAccountAccessTokenExecutor.scheduleWithFixedDelay(officialAccountAccessTokenTask, 5, 5, TimeUnit.MINUTES);
+
+            ScheduledExecutorService jsTicketExecutor = Executors.newSingleThreadScheduledExecutor();
+            Runnable jsTicketTask = new Runnable() {
+                @Override
+                public void run() {
+                    int maxRetries = 3;
+                    int retryCount = 0;
+                    while (retryCount < maxRetries) {
+                        try {
+                            WeChatUtil.refreshJsapiTicket();
+                            break;
+                        } catch (Exception e) {
+                            log.error("定时刷新JSTicket失败,异常信息:", e);
+                            retryCount++;
+                            if (retryCount == maxRetries) {
+                                log.error("定时刷新JSTicket方法调用已达到最大重试次数");
+                            } else {
+                                log.error("正在进行第 " + retryCount + " 次重试");
+                            }
+                        }
+                    }
+                }
+            };
+            // 设置定时器，任务执行完成后等待2小时，然后再执行下一次任务
+            jsTicketExecutor.scheduleWithFixedDelay(jsTicketTask, 2, 2, TimeUnit.HOURS);
+        }
     }
 }
