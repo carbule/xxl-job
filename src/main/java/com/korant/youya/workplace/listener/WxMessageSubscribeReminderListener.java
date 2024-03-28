0ca5ac7f8ac0edbd03b0f76a62283189b1e33b43
周志峰 <zhouzhf@korant.cn>
2024-03-28 15:32:19 +0800

    [IMP] rabbitmq修改虚拟主机配置

--- src/main/java/com/korant/youya/workplace/listener/WxMessageSubscribeReminderListener.java
+++ src/main/java/com/korant/youya/workplace/listener/WxMessageSubscribeReminderListener.java
@@ -20,7 +20,6 @@ import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
@@ -33,7 +32,6 @@ import java.util.Optional;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-@Profile({"test", "prod"})
 public class WxMessageSubscribeReminderListener {
 
     public static final String TIPS = "请在截止前完成预约，否则将视为放弃！（已确认的请忽略此消息）";
